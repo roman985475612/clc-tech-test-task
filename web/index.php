0685b9c0024084d5d0a8de112e26fbd5711ed20e
Roman <roman985475612@gmail.com>
2021-04-27 11:29:18 +0300

    Added favicon

--- web/index.php
+++ web/index.php
@@ -1,14 +1,14 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG'));
+defined('YII_ENV') or define('YII_ENV', 'dev');
 
 $config = require __DIR__ . '/../config/web.php';
 
