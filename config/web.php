27f720abdce133cdabb52cef84951bf4d4769781
Roman <roman985475612@gmail.com>
2021-04-27 11:18:27 +0300

    Changed config

--- config/web.php
+++ config/web.php
@@ -15,7 +15,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'j-Hcqv3ad15te6LUwlvvlxFmDdKlBF9x',
+            'cookieValidationKey' => env('COOKIE_VALIDATION_KEY'),
             'baseUrl' => '',
         ],
         'cache' => [
